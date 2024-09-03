59080be82d25d418d9c9c4ccaf855fc89015fa09
Avinash H. Duduskar <2946372+Strykar@users.noreply.github.com>
2024-09-04 01:46:08 +0530

    Update config.py

--- smokeping/smokepeer/config.py
+++ smokeping/smokepeer/config.py
@@ -2,3 +2,4 @@ ISO_CSV_PATH = '/path/to/local/iso_country_codes.csv'
 AIRPORT_CSV_PATH = '/path/to/local/airport_codes.csv'
 DB_FILE = '/tmp/livepeer_ips.db'
 SUPPORTED_LARGE_COUNTRIES = ['RUS', 'CAN', 'CHN', 'USA', 'BRA', 'AUS', 'IND', 'ARG']
+LPT_DNS = 'livepeer.multiorch.com'  # Default DNS name for Livepeer
